72f3158572c42172ed5ac1bb2dad6f4b375478ff
ScienceWater <vtufo@sbcglobal.net>
2022-04-10 20:28:44 -0400

    satd resolution 8

--- src/org/jhotdraw/samples/javadraw/JavaDrawApp.java
+++ src/org/jhotdraw/samples/javadraw/JavaDrawApp.java
@@ -4,7 +4,7 @@
  * Project:		JHotdraw - a GUI framework for technical drawings
  *				http://www.jhotdraw.org
  *				http://jhotdraw.sourceforge.net
- * Copyright:	© by the original author(s) and all contributors
+ * Copyright:	ï¿½ by the original author(s) and all contributors
  * License:		Lesser GNU Public License (LGPL)
  *				http://www.opensource.org/licenses/lgpl-license.html
  */
@@ -30,7 +30,7 @@ import java.net.URL;
 /**
  * @version <$CURRENT_VERSION$>
  */
-public  class JavaDrawApp extends MDI_DrawApplication {
+public  class JavaDrawApp extends MDIDrawApplication {
 
 	private Animator            fAnimator;
 	private static String       fgSampleImagesPath = "/org/jhotdraw/samples/javadraw/sampleimages";
