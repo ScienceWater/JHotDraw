6d1cfdf81f4229d9333aa90ad7bb8de369685ecc
ScienceWater <vtufo@sbcglobal.net>
2022-04-10 20:27:03 -0400

    satd comment 7

--- src/org/jhotdraw/contrib/PolygonTool.java
+++ src/org/jhotdraw/contrib/PolygonTool.java
@@ -4,7 +4,7 @@
  * Project:		JHotdraw - a GUI framework for technical drawings
  *				http://www.jhotdraw.org
  *				http://jhotdraw.sourceforge.net
- * Copyright:	© by the original author(s) and all contributors
+ * Copyright:	ï¿½ by the original author(s) and all contributors
  * License:		Lesser GNU Public License (LGPL)
  *				http://www.opensource.org/licenses/lgpl-license.html
  */
@@ -74,6 +74,7 @@ public class PolygonTool extends AbstractTool {
 	public void mouseDown(MouseEvent e, int x, int y) {
 		super.mouseDown(e,x,y);
 		// replace pts by actual event pts
+		//FIXME: x and y never get used, why reassign?
 		x = e.getX();
 		y = e.getY();
 
